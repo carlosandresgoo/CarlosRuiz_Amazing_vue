2a429e7011fb7740dea289db64aae73433b788b4
Carlos Ruiz <carlosandresgoo@gmail.com>
2023-03-16 17:02:14 -0500

    tarea 4-2

--- js/stats.js
+++ js/stats.js
@@ -3,7 +3,6 @@ const { createApp } = Vue
 const app = createApp({
     data(){
         return {
-    // Inicializamos las variables
         eventoseventosFuturos: [],
         eventosPasados: [],
         mayorAsistencia: "",
@@ -20,7 +19,6 @@ const app = createApp({
             this.eventosFuturos = data.events.filter(event => event.date >= data.currentDate);
             this.eventosPasados = data.events.filter(event => event.date < data.currentDate);
 
-            //tabla 1
             this.mayorAsistencia = this.eventosPasados.sort((evento1, evento2) => {
                 return (
                     (evento1.assistance / evento1.capacity) * 100 -
@@ -39,9 +37,7 @@ const app = createApp({
                 return ( evento1.capacity - evento2.capacity);
             }).slice(-1)[0]
             
-            //tabla 2
             this.eventosFuturos.forEach(event => {
-                // Si la categoría no existe la creamos
                 if (!this.categories[event.category]) {
                     this.categories[event.category] = {
                         price: 0,
@@ -49,15 +45,12 @@ const app = createApp({
                         capacity: 0,
                     }
                 }
-                // Sumamos los ingresos la asistencia y la capacity a la categoría correspondiente
                 this.categories[event.category].price += event.price * event.estimate
                 this.categories[event.category].capacity += event.capacity
                 this.categories[event.category].estimate += event.estimate
             })
 
-            //tabla 3
             this.eventosPasados.forEach(event => {
-                // Si la categoría no existe la creamos
                 if (!this.categories2[event.category]) {
                     this.categories2[event.category] = {
                         price: 0,
@@ -65,7 +58,6 @@ const app = createApp({
                         capacity: 0,
                     }
                 }
-                // Sumamos los ingresos la asistencia y la capacity a la categoría correspondiente
                 this.categories2[event.category].price += event.price * event.assistance
                 this.categories2[event.category].capacity += event.capacity
                 this.categories2[event.category].assistance += event.assistance
